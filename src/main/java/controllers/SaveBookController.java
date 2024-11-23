260536f94d9be9e506537a42bd1cf87835c80b72
Enis Shallci <enisshallci@gmail.com>
2024-11-23 17:17:28 +0100

    fixing combo box

--- src/main/java/controllers/SaveBookController.java
+++ src/main/java/controllers/SaveBookController.java
@@ -1,6 +1,26 @@
 package controllers;
 
-public class SaveBookController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SaveBookController implements Initializable {
+
+    @FXML
+    private ComboBox comb;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        ObservableList<String> lista = FXCollections.observableArrayList("Science Fiction", "Fantasy","History","Philosophy", "Science", "Travel", "Biography");
+        comb.setItems(lista);
+        comb.setValue(lista.get(0));
+    }
 
 }
